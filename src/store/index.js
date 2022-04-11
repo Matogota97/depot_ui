3f0f63a2e274ce0e5343e2fb319952d436990f65
Marley97 <hermesmarleyngenda@gmail.com>
2022-04-11 16:43:17 +0200

    cart

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,15 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 export default new Vuex.Store({
-    state: {
-            user: null,
-            url : "http://127.0.0.1:8000/api",
-            notification:{type:"", message:""},
-            groups: null,
-            client_courant:{},
-            vendeur_courant:{},
-            produit_courant:{},
-            carts:{}
-        }
+	state: {
+			user: null,
+			url : "http://127.0.0.1:8000/api",
+			notification:{type:"", message:""},
+			groups: null,
+			client_courant:{},
+			vendeur_courant:{},
+			produit_courant:{},
+			carts:[],
+			products:[]
+		}
 })
